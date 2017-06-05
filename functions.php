175d0b9d4ecebd1fc3a46b281f433a205817257a
Simon Ring <simon@Elins-MacBook-Pro.local>
2017-06-05 19:41:11 +0200

    Working theme

--- functions.php
+++ functions.php
@@ -8,4 +8,8 @@ function affiliate_menus() {
 }
 add_action( 'init', 'affiliate_menus' );
 
+// Enqueue styles and scripts
+wp_enqueue_style( 'style', get_template_directory_uri().'/dist/styles/theme.min.css', array(), time() );
+wp_enqueue_script( 'script', get_template_directory_uri() . '/dist/scripts/main.min.js', array ( 'jquery' ), time(), true);
+
 ?>
